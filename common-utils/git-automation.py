7324aa7d7a77c81799a053337242f0b488092a7f
阿樊提 <fanjiahe2000@163.com>
2015-08-02 23:51:27 +0800

    Update git-automation.py

--- common-utils/git-automation.py
+++ common-utils/git-automation.py
@@ -1,20 +1,24 @@
 import envoy
 from selenium import webdriver
 import time
+import os
 
 def main():
+    if os.path.exists('wonderfan.github.io'):
+        envoy.run("rm -rf wonderfan.github.io")
+
     repo = 'git@github.com:wonderfan/wonderfan.github.io.git'
     envoy.run('git clone '+ repo)
     cwd = "wonderfan.github.io"
     get_screenshot()
     envoy.run("git add --all ",cwd=cwd)
     envoy.run('git commit -m "submit the changes"',cwd=cwd)
-    envoy.run("git push origin master")
+    envoy.run("git push origin master",cwd=cwd)
 
 def get_screenshot():
     driver = webdriver.Firefox()
-    driver.get("http://www.msn.com")
-    file_name = "".join(["test-",str(int(time.time())),'.png'])
+    driver.get("http://www.bloomberg.com")
+    file_name = "".join(["finance-",str(int(time.time())),'.png'])
     driver.save_screenshot("wonderfan.github.io/"+file_name)
     driver.close()
 

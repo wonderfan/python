c9458ce46fe9eb553b55c28c4b7fc24167f44ad9
阿樊提 <fanjiahe2000@163.com>
2015-08-02 22:13:55 +0800

    use selenium and git to output content

--- common-utils/git-automation.py
+++ common-utils/git-automation.py
@@ -7,7 +7,7 @@ def main():
     cwd = "python"
     envoy.run("rm -f wonderfan.py",cwd=cwd)
     envoy.run("git add * ",cwd=cwd)
-    envoy.run('git commit -m "this is test"',cwd=cwd)
+    envoy.run('git commit -m "submit the changes"',cwd=cwd)
     envoy.run("git push origin master")
 
 def get_screenshot():

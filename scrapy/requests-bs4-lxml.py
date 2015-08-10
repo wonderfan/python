d29084c47d6db96e41bf9b6569c5dfc47bcfdfe0
阿樊提 <fanjiahe2000@163.com>
2015-08-10 23:34:19 +0800

    Update requests-bs4-lxml.py

--- scrapy/requests-bs4-lxml.py
+++ scrapy/requests-bs4-lxml.py
@@ -1,2 +1,8 @@
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html_doc, 'html.parser')
+
+BeautifulSoup(markup, "lxml") #use different html parser
+BeautifulSoup(markup, "html5lib")
+
+for link in soup.find_all('a'):
+    print(link.get('href'))

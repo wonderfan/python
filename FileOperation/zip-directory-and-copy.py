515cdb3637a8af12293bd496fb71be99c067e68c
阿樊提 <fanjiahe2000@163.com>
2015-08-10 09:53:27 +0800

    Update zip-directory-and-copy.py

--- FileOperation/zip-directory-and-copy.py
+++ FileOperation/zip-directory-and-copy.py
@@ -4,7 +4,7 @@ def main():
     base_dir = 'C:\zenith\monitor\Deployment and Automation\chef_stuff\chef_repo\cookbooks\sce'
     os.chdir(base_dir)
     file_name = shutil.make_archive('monitor','zip',os.curdir,os.curdir+os.path.sep+'monitor')
-    shutil.move(file_name,'C:\Users\IBM_ADMIN\Downloads')   
+    shutil.move(file_name,'C:\Users\ADMIN\Downloads')   
 
 if __name__ == '__main__':
     main()

cdd026be1c9a31cdf6459479217a2c83d05b29d5
wonderfan <fanjiahe2000@163.com>
2016-06-05 09:58:10 +0800

    Create python_setup.sh

---
+++ Shell/python_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install xvfb
+sudo apt-get install firefox
+
+source ~/workspace/env/bin/activate
+
+pip install selenium
+
+pip install xvfbwrapper

9ec6ae2416799f48c90079ddc7050f0e7f9b145e
Afanti <fanjiahe2000@163.com>
2017-12-07 18:32:10 +0800

    Update youtube-simple.py

--- scrapy/youtube-simple.py
+++ scrapy/youtube-simple.py
@@ -1,26 +1,23 @@
 import unittest
 import csv
-import requests
-import re
 import uuid
+import requests
 from lxml import etree
-from io import StringIO, BytesIO
+from io import StringIO
 
 class SimpleTest(unittest.TestCase):
     
     def test_download(self):
         url = "http://www.youtube.com/results?q=python regular expression"
         response = requests.get(url)
-        parser = etree.HTMLParser()
-        tree   = etree.parse(StringIO(response.text), parser)
+        tree   = etree.parse(StringIO(response.text), etree.HTMLParser())
         link_list = tree.xpath("//a[contains(@href,'watch')]")
         uid = str(uuid.uuid4())
         print(">>> "+ uid)
         f = csv.writer(open(uid + ".csv", "w"))
-        f.writerow(["#####     Name     #####","#####     Link     #####"])  
         for element in link_list:
             if element.text is not None:
-                f.writerow([element.text.encode('utf-8'),"  http://www.youtube.com" + element.attrib.get("href")])
+                f.writerow([element.text.encode('utf-8'),"    http://www.youtube.com" + element.attrib.get("href")])
    
 if __name__ == '__main__':
     unittest.main() 
